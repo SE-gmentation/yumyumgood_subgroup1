39ab4085930788124b546fcc3956617361561255
koeunseo <rhdmstj17@cau.ac.kr>
2021-05-31 00:47:50 +0900

    [style] ssd uc-5 docs에 맞게 클래스 구조화 완료

--- display.py
+++ display.py
@@ -16,88 +16,70 @@ restaurant =""
 db = client.meal
 globalDB = db.cham
 
+class QRcode:
+    def create_qrcode(self,orders):
+        global restaurant
+        # 현재 시간 가져오기
+        current = datetime.now()
+        # 10분 후 시간 가져오기
+        ten_minutes_later = current + timedelta(minutes=10)
+        ten_minutes_later = ten_minutes_later.strftime('%H시 %M분')
+        
+        print("\n            [. . . . . . loading . . . . . .]\n")
+        time.sleep(1)
+        QR=qrcode.make(orders) # 주문 내역에 대한 정보 qr코드에 저장
 
-def create_qrcode(orders):
-    global restaurant
-    # 현재 시간 가져오기
-    current = datetime.now()
-    # 10분 후 시간 가져오기
-    ten_minutes_later = current + timedelta(minutes=10)
-    ten_minutes_later = ten_minutes_later.strftime('%H시 %M분')
-    
-    print("\n            [. . . . . . loading . . . . . .]\n")
-    time.sleep(1)
-    QR=qrcode.make(orders) # 주문 내역에 대한 정보 qr코드에 저장
+        print("======================================================================")
+        print("                      QR이 정상적으로 생성되었습니다.")
+        print("{}까지 {} 학식당에 가서 큐알코드를 보여주세요 (유효시간:10분)".format(ten_minutes_later,restaurant)) # 아직 학식당 안불러옴 나중에 디비에서 빼장
+        print("======================================================================\n")
+        
+        num=int(input("QR이미지를 저장하시겠습니까? (1) yes (2) no : "))
 
-    print("======================================================================")
-    print("                      QR이 정상적으로 생성되었습니다.")
-    print("{}까지 {} 학식당에 가서 큐알코드를 보여주세요 (유효시간:10분)".format(ten_minutes_later,restaurant)) # 아직 학식당 안불러옴 나중에 디비에서 빼장
-    print("======================================================================\n")
-    
-    num=int(input("QR이미지를 저장하시겠습니까? (1) yes (2) no : "))
+        if num==1:
+            QR.save(("qr_image.png"))
 
-    if num==1:
-        QR.save(("qr_image.png"))
+        print("\n주문이 성공적으로 접수되었습니다!!")
+        print("첫 화면으로 돌아갑니다.")
+        time.sleep(2)
 
-    print("\n주문이 성공적으로 접수되었습니다!!")
-    print("첫 화면으로 돌아갑니다.")
-    time.sleep(2)
-
-    cls()
-    UC4_controller()
-    
-# qrcode 생성과 동시에 db접근하여 재고 수량 업데이트
-def stock_update(orders):
-    for order in orders:
-        temp = order[0]
-        temp[2] -= order[1]
-        globalDB.update_one({"date":date_token[0]}, {"$set":{temp[3]:temp}},  upsert=True)
-
-def pay_list(orders):
-    total=0
-    costs=[]
-    for order in orders:
-        cost=int(order[0][1][0]+order[0][1][2:-1])
-        costs.append(cost)
-        total+=cost*order[1]
-
-    discount=total//10 # 일단 회원할인 전체 금액의 10프로 할인이라고 가정
-    pay=total-discount
-
-    print("=========================[주문 내역]=========================")
-    # 회원인지 비회원인지는 이미 등록되어 온 상태라고 가정한다.
-    # 총금액, 회원할인금액, 결제 금액까지 보여주고 / 결제하러가기 클릭 
-    total_num =0
-    for i, order in enumerate(orders):
-        if(order[1]==0): continue
-        print("{}".format(order[0][0]))
-        print("                                     수량:{}개 | 가격:{}원 |".format(order[1],costs[i]*order[1]))
-        total_num=total_num+order[1]
-        print("-------------------------------------------------------------")
-    print(" 총수량 | {}".format(total_num))
-    print("=============================================================")
-    print("\n\n")
-
-
-    print("=========================[결제 내역]=========================")
-    print("총금액    |                                     {}원".format(total))
-    print("회원 할인 ㅣ                                    {}원".format(discount))
-    print("결제 금액 |                                     {}원".format(pay))
-    print("=============================================================")
-    
-    print("\n")
-    print("({}). 뒤로 가기".format(1))
-    print("\n")
-    print("({}). 결제 하러 가기 (QR코드) \n".format(2))
-
-    go=int(input("결제를 원하시면 2번을 선택해주세요. :"))
-    if(go == 1): # 뒤로 가기 (어떤 메뉴도 클릭하지 않았다.)
-        print("이전 화면으로 돌아갑니다.")
         cls()
-        return 0 
-    else:
-        stock_update(orders)
-        create_qrcode(orders)
+        UC4_controller()
+    
+
+
+class Calculator:
+    def __init__(self,orders):
+        self.total=0
+        self.costs=[]
+        self.orders=orders
+    
+    def calculate_amount(self):
+        for order in self.orders:
+            cost=int(order[0][1][0]+order[0][1][2:-1])
+            self.costs.append(cost)
+            self.total+=cost*order[1]
+        return self.costs,self.total
+
+    def calculate_discount(self):
+        return self.total//10
+
+    def calculate_pay(self,discount):
+        return self.total-discount
+
+def UC5_controller(orders):
+    calculator = Calculator(orders)
+    costs,total = calculator.calculate_amount()
+    discount = calculator.calculate_discount()
+    pay=calculator.calculate_pay(discount)
+
+    interface=PM_Interface()
+    interface.UC5_interface(costs,total,discount,pay,orders)
+
+    button = Button_click()
+    button.payment_process(orders)
+
+    
 
 class Order: # 얘는 uc4의 메뉴리스트 아니고, uc2의 주문(메뉴리스트+담은수량)
     def __init__(self):
@@ -132,14 +114,23 @@ def cls():
     os.system('cls' if os.name=='nt' else 'clear' )
 
 class Menu:
+    def __init__(self):
+        self.menu=[]
+
     def today_menu(self,picked,meal_time): #오늘(지금) 구매 가능한 메뉴 리스트 정보 담는 함수 따로 분기
-        menu = []
         for time in picked:    
             if(meal_time in time or "간식" in time):
                 meal = picked[time] # meal[0] = 메뉴 이름, meal[1] = 가격, meal[2] = 재고, meal[3] = 카테고리
                 meal.append(time)
-                menu.append(meal)
-        return menu
+                self.menu.append(meal)
+        return self.menu
+
+    # qrcode 생성과 동시에 db접근하여 재고 수량 업데이트
+    def stock_update(self,orders):
+        for order in orders:
+            temp = order[0]
+            temp[2] -= order[1]
+            globalDB.update_one({"date":date_token[0]}, {"$set":{temp[3]:temp}},  upsert=True)
 
 class PM_Interface:
     def UC2_interface(self,tmp_cart,order):
@@ -186,6 +177,34 @@ class PM_Interface:
         print("3. 종료하기\n")
 
         return meal_time
+
+    def UC5_interface(self,costs,total,discount,pay,orders):
+        print("=========================[주문 내역]=========================")
+        # 회원인지 비회원인지는 이미 등록되어 온 상태라고 가정한다.
+        # 총금액, 회원할인금액, 결제 금액까지 보여주고 / 결제하러가기 클릭 
+        total_num =0
+        for i, order in enumerate(orders):
+            if(order[1]==0): continue
+            print("{}".format(order[0][0]))
+            print("                                     수량:{}개 | 가격:{}원 |".format(order[1],costs[i]*order[1]))
+            total_num=total_num+order[1]
+            print("-------------------------------------------------------------")
+        print(" 총수량 | {}".format(total_num))
+        print("=============================================================")
+        print("\n\n")
+
+
+        print("=========================[결제 내역]=========================")
+        print("총금액    |                                     {}원".format(total))
+        print("회원 할인 ㅣ                                    {}원".format(discount))
+        print("결제 금액 |                                     {}원".format(pay))
+        print("=============================================================")
+        
+        print("\n")
+        print("({}). 뒤로 가기".format(1))
+        print("\n")
+        print("({}). 결제 하러 가기 (QR코드) \n".format(2))
+
 
 class Button_click:
     def available_now(self,meal_time):
@@ -256,7 +275,6 @@ class Button_click:
                     print("메뉴를 1개 이상 담아주세요.")
                     time.sleep(0.5)
                 else:
-                    print("?")
                     UC2_controller(tmp_cart.get_tmp_cart())
             else:
                 tmp_cart.compute_total_num(menu[ret-1])
@@ -274,18 +292,40 @@ class Button_click:
         elif(ret == c+3):
             time.sleep(0.5)
             cls()
-            pay_list(order.get_order()) # 결제 하러 가기 (주문 내역 및 결제버튼 확인하는 화면으로 이동)
+            UC5_controller(order.get_order())
+            # pay_list(order.get_order()) # 결제 하러 가기 (주문 내역 및 결제버튼 확인하는 화면으로 이동)
             pass
         else:
             num = int(input("원하는 만큼 개수 조절을 해주세요 : "))
             # while((num<0 or num>10) or num > meal[2] ):
-            while((num<0 or num>10)):
-                if(num<0 or num>10): # 개수 조절 범위 : 0~10
-                    num = int(input("개수 조절 허용 범위는 0~10입니다.다시 입력해주세요 : "))
-                # elif(num > meal[2]): # 재고 수량보다 넘겼을 경우
-                #     num = int(input("재고({})보다 많은 수량을 주문할 수 없습니다. 다시 입력해주세요: ".format(meal[2])))
+
+            data_filter=Data_filter()
+            data_filter.filtering(num)
+            
             order.control_menu_num(ret-1,num)
             # -> 특정 메뉴 클릭, 개수 입력 / 단 0,10 사이로만 가능함
+
+    def payment_process(self,orders):
+        go=int(input("결제를 원하시면 2번을 선택해주세요. :"))
+        if(go == 1): # 뒤로 가기 (어떤 메뉴도 클릭하지 않았다.)
+            print("이전 화면으로 돌아갑니다.")
+            cls()
+            return 0 
+        else:
+            menu = Menu()
+            menu.stock_update(orders)
+
+            qr = QRcode()
+            qr.create_qrcode(orders)
+
+class Data_filter:
+    def filtering(self,num):
+        while((num<0 or num>10)):
+            if(num<0 or num>10): # 개수 조절 범위 : 0~10
+                num = int(input("개수 조절 허용 범위는 0~10입니다.다시 입력해주세요 : "))
+            # elif(num > meal[2]): # 재고 수량보다 넘겼을 경우
+            #     num = int(input("재고({})보다 많은 수량을 주문할 수 없습니다. 다시 입력해주세요: ".format(meal[2])))
+        return num
 
 class Tmp_cart:
     def __init__(self):
